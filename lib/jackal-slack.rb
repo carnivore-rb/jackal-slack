a7d71f3448b86afcf6e1178f386464c23cc8fb19
Chris Roberts <code@chrisroberts.org>
2015-06-10 09:18:54 -0700

    Add categorization on service registration

--- lib/jackal-slack.rb
+++ lib/jackal-slack.rb
@@ -12,8 +12,10 @@ require 'jackal-slack/version'
 Jackal.service(
   :slack,
   :description => 'Send messages to Slack',
+  :category => :notifier,
   :configuration => {
     :webhook_url => {
+      :type => :string,
       :description => 'Webhook URL to send messages'
     }
   }
